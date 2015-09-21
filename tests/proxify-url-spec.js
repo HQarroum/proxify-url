2a51b36060c25161ce4cd5ac53b85344b32e57af
Halim Qarroum <hqarroum@awox.com>
2015-09-21 09:21:05 +0200

    Testing the proxify function with a querystring URL

--- tests/proxify-url-spec.js
+++ tests/proxify-url-spec.js
@@ -3,7 +3,9 @@ var proxify = require('../lib');
 var url     = require('url');
 
 describe('The proxify function', function () {
-
+    
+    var baseUrl = 'https://query.yahooapis.com/v1/public/yql?';
+    
     /**
      * Invalid input test.
      */
@@ -17,7 +19,7 @@ describe('The proxify function', function () {
      * Transforming the URL using default options test.
      */
     it('should be able to proxify an URL using default options', function () {
-        var result = 'https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20json%20WHERE%20url%3D%22https%3A%2F%2Fapi.github.com%2Fusers%2Foctocat%22&format=json&jsonCompat=new';
+        var result = baseUrl + 'q=SELECT%20*%20FROM%20json%20WHERE%20url%3D%22https%3A%2F%2Fapi.github.com%2Fusers%2Foctocat%22&format=json&jsonCompat=new';
         proxify('https://api.github.com/users/octocat').should.be.eql(result);
     });
 
@@ -25,7 +27,7 @@ describe('The proxify function', function () {
      * Custom output format test.
      */
     it('should be able to proxify an URL using a custom output format options', function () {
-        var result = 'https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20json%20WHERE%20url%3D%22https%3A%2F%2Fapi.github.com%2Fusers%2Foctocat%22&format=xml';
+        var result = baseUrl + 'q=SELECT%20*%20FROM%20json%20WHERE%20url%3D%22https%3A%2F%2Fapi.github.com%2Fusers%2Foctocat%22&format=xml';
         proxify('https://api.github.com/users/octocat', { outputFormat: 'xml', jsonCompat: false }).should.be.eql(result);
     });
 
@@ -33,15 +35,20 @@ describe('The proxify function', function () {
      * Custom input format test.
      */
     it('should be able to proxify an URL using a custom input format options', function () {
-        var result = 'https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20xml%20WHERE%20url%3D%22https%3A%2F%2Fapi.github.com%2Fusers%2Foctocat%22&format=json&jsonCompat=new';
+        var result = baseUrl + 'q=SELECT%20*%20FROM%20xml%20WHERE%20url%3D%22https%3A%2F%2Fapi.github.com%2Fusers%2Foctocat%22&format=json&jsonCompat=new';
         proxify('https://api.github.com/users/octocat', { inputFormat: 'xml' }).should.be.eql(result);
     });
-
+    
+    it('should be able to proxify an URL with query parameters', function () {
+	var result = baseUrl + 'q=SELECT%20*%20FROM%20json%20WHERE%20url%3D%22https%3A%2F%2Fapi.github.com%2Fsearch%2Frepositories%3Fq%3Dproxify-url%26order%3Dasc%22&format=json&jsonCompat=new';
+	proxify('https://api.github.com/search/repositories?q=proxify-url&order=asc').should.be.eql(result);
+    });
+    
     /**
      * JSONP callback test.
      */
     it('should be able to proxify an URL using a JSONP callback', function () {
-        var result = 'https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20json%20WHERE%20url%3D%22https%3A%2F%2Fapi.github.com%2Fusers%2Foctocat%22&format=json&jsonCompat=new&callback=foo';
+        var result = baseUrl + 'q=SELECT%20*%20FROM%20json%20WHERE%20url%3D%22https%3A%2F%2Fapi.github.com%2Fusers%2Foctocat%22&format=json&jsonCompat=new&callback=foo';
         proxify('https://api.github.com/users/octocat', { callback: 'foo' }).should.be.eql(result);
     });
 
